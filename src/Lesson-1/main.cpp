c79db6502a73891ca3041559b683df0bf21c60a9
m-konarzewski <michal.konarzewski.office@gmail.com>
2026-09-18 22:31:09 +0200

    apply clang format changes

--- src/Lesson-1/main.cpp
+++ src/Lesson-1/main.cpp
@@ -1,13 +1,12 @@
+#include <iostream>
+
 #include <SDL3/SDL.h>
 
 extern "C" {
 #include <libavutil/avutil.h>
 }
 
-#include <iostream>
-
-int main()
-{
+int main() {
     std::cout << "FFmpeg libavutil: " << av_version_info() << '\n';
     std::cout << "SDL3: " << SDL_GetVersion() << '\n';
 }
